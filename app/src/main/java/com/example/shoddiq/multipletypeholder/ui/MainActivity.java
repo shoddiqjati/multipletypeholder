270232216539ef2c0533abd02a6ebe32b148ccee
shoddiqjati <shoddiq.jati@gmail.com>
2017-11-03 14:35:53 +0700

    fix recycler

--- app/src/main/java/com/example/shoddiq/multipletypeholder/ui/MainActivity.java
+++ app/src/main/java/com/example/shoddiq/multipletypeholder/ui/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements MainView {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         LinearLayoutManager manager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(manager);
+        recyclerView.setNestedScrollingEnabled(false);
     }
 
     @Override
